e30a31680448e66810765631d665a52bda195a20
Phong Ngo <phongngo91@gmail.com>
2020-02-24 12:59:45 -0500

    Fix merge conflict

--- routes/api/fortunes.js
+++ routes/api/fortunes.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const express = require("express");
 const router = express.Router();
 const mongoose = require("mongoose");
@@ -54,28 +53,3 @@ router.post(
 );
 
 module.exports = router;
-=======
-//retrieve a specific fortune for sepcific user.
-
-const express = require('express');
-const router = express.Router();
-const mongoose = require('mongoose');
-const passport = require('passport');
-
-const Fortune = require('../../models/Fortune');
-const Calculation = require('../../models/calculation')
-const validateFortuneInput = require('../../validation/fortunes');
-
-router.get('/user/:user_id', (req, res) => {
-  Calculation.find({user: req.params.user})
-})
-
-router.get('/fortune')
-
-router.get('/:id', (req, res) => {
-  Fortune.findById(req.params.id)
-    .then(fortune => res.json(fortune))
-    .catch(err => 
-        res.status(404).json({ nofortunefound: 'No fortune found with this ID'}))
-});
->>>>>>> 7bed68ce08adbfb45b4bdb977d7816cc739e4ed2
