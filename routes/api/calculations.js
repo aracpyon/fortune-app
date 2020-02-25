05bab513f10e1180c9cb433699b7b8a4b291be76
Phong Ngo <phongngo91@gmail.com>
2020-02-25 15:02:33 -0500

    Add create calculation route

--- routes/api/calculations.js
+++ routes/api/calculations.js
@@ -3,42 +3,106 @@ const router = express.Router();
 const mongoose = require("mongoose");
 const passport = require("passport");
 const jwt = require("jsonwebtoken");
-const Calculation = require('../../models/Calculations');
-const ZodiacCalc = require('../../frontend/src/util/zodiac_calc_util');
+const Calculation = require("../../models/Calculations");
+const ZodiacCalc = require("../../frontend/src/util/zodiac_calc_util");
+const Fortune = require("../../models/Fortune");
 
 router.post("/", (req, res) => {
+  const user_1 = {
+    _id: req.body["user_1._id"],
+    zodiac: req.body["user_1.zodiac"],
+    kids: parseInt(req.body["user_1.kids"]),
+    marriage: parseInt(req.body["user_1.marriage"]),
+    personality: parseInt(req.body["user_1.personality"])
+  };
 
-    const user_1 = {
-        _id: req.body['user_1._id'],
-        zodiac: req.body['user_1.zodiac'],
-        kids: parseInt(req.body['user_1.kids']),
-        marriage: parseInt(req.body['user_1.marriage']),
-        personality: parseInt(req.body['user_1.personality'])
-    };
+  const user_2 = {
+    _id: req.body["user_2._id"],
+    zodiac: req.body["user_2.zodiac"],
+    kids: parseInt(req.body["user_2.kids"]),
+    marriage: parseInt(req.body["user_2.marriage"]),
+    personality: parseInt(req.body["user_2.personality"])
+  };
 
-    const user_2 = {
-        _id: req.body['user_2._id'],
-        zodiac: req.body['user_2.zodiac'],
-        kids: parseInt(req.body['user_2.kids']),
-        marriage: parseInt(req.body['user_2.marriage']),
-        personality: parseInt(req.body['user_2.personality'])
-    };
+  // Calculates the compatibility based on zodiac
+  const percentCompatZodiac = ZodiacCalc[user_1.zodiac][user_2.zodiac];
 
-    const percentCompatZodiac = ZodiacCalc[user_1.zodiac][user_2.zodiac];
-    debugger
+  // Calculates the compatibility based on personal information
+  let total_per = Math.abs(user_1.kids - user_2.kids);
+  total_per += Math.abs(user_1.marriage - user_2.marriage);
+  total_per += Math.abs(user_1.personality - user_2.personality);
+  const final_score = (total_per / 4) * 100;
 
+  // Averages both compatibility
+  const final_percentage = (percentCompatZodiac + final_score) / 2;
 
-    const newCalculation = new Calculation({
-        userOne: req.body.userOne,
-        userTwo: req.body.userTwo,
-        fortuneId: req.body.fortuneId
-        // percentage 
-    });
-    newCalculation.save().then(calculation => res.json(calculation));
+  if (final_percentage < 50) {
+    Fortune.aggregate([
+      { $match: { favorability: "negative" } },
+      { $sample: { size: 1 } }
+    ])
+      .then(fortune => {
+        const newCalculation = new Calculation({
+          user_1: user_1._id,
+          user_2: user_2._id,
+          fortune_id: fortune[0]._id,
+          percentage: final_percentage
+        });
+
+        newCalculation
+          .save()
+          .then(calculation => {
+            return res.json(calculation);
+          })
+          .catch(err => res.json(err));
+      })
+      .catch(err => res.json(err));
+  } else if (final_percentage > 50 && final_percentage > 80) {
+    Fortune.aggregate([
+      { $match: { favorability: "positive" } },
+      { $sample: { size: 1 } }
+    ])
+      .then(fortune => {
+        const newCalculation = new Calculation({
+          user_1: user_1._id,
+          user_2: user_2._id,
+          fortune_id: fortune[0]._id,
+          percentage: final_percentage
+        });
+        newCalculation
+          .save()
+          .then(calculation => {
+            return res.json(calculation);
+          })
+          .catch(err => res.json(err));
+      })
+      .then(err => res.json(err));
+  } else {
+    Fortune.aggregate([
+      { $match: { favorability: "super positive" } },
+      { $sample: { size: 1 } }
+    ])
+      .then(fortune => {
+        const newCalculation = new Calculation({
+          user_1: user_1._id,
+          user_2: user_2._id,
+          fortune_id: fortune[0]._id,
+          percentage: final_percentage
+        });
+
+        newCalculation
+          .save()
+          .then(calculation => {
+            return res.json(calculation);
+          })
+          .catch(err => res.json(err));
+      })
+      .catch(err => res.json(err));
+  }
 });
 
 router.get("/", (req, res) => {
-    req.find().then(allCalc => res.json(allCalc) );
+  Calculation.find().then(allCalc => res.json(allCalc));
 });
 
 module.exports = router;
