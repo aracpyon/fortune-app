7f8d2f83ed38d7bb910d8c197a3038454f248872
jhiggs <higgsj82@gmail.com>
2020-02-25 23:24:52 -0500

    Add auth and protected routes

--- frontend/src/components/app.js
+++ frontend/src/components/app.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Switch } from 'react-router-dom';
+import { AuthRoute, ProtectedRoute } from '../util/route_utils';
 import LoginFormContainer from './sessions/login_form_container';
 import RegistrationFormContainer from './sessions/registration_form_container';
 
@@ -7,8 +8,8 @@ const App = () => (
   <div className="main-app-container">
     <h1>Hey</h1>
     <Switch>
-      <LoginFormContainer />
-      <RegistrationFormContainer />
+      <AuthRoute path="/login" component={LoginFormContainer}/>
+      <AuthRoute path="/signup" component={RegistrationFormContainer}/>
     </Switch>
   </div>
 );
