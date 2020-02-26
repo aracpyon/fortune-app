5af7a12ecfce2ffa3f58d06f4a397b23e1e80630
Melody <luomel@outlook.com>
2020-02-26 16:34:33 -0500

    Finish Splash Css

--- frontend/src/components/splash/main_spash.js
+++ frontend/src/components/splash/main_spash.js
@@ -7,10 +7,11 @@ class SplashPage extends React.Component {
     render() {
         return (
             <div className="main-splash-container">
-              
+            
                 <div className="center-image-section"> 
                     <img className="fortune-cookie-img" src="/fortune_couple.png" alt="fortune-cookie" />
                 </div>
+                <div><h1>Get Your Fortune Today</h1></div>
                 <NavLink className="Signup-link" to="/signup">Sign Up</NavLink>
                 <NavLink className="Login-link" to="/login">Log In</NavLink>
             </div>
