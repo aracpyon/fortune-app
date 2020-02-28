2b49ed96f32f7d8546330deb5ff35ea8b423b049
Phong <phongngo91@gmail.com>
2020-02-28 11:17:42 -0500

    Delete keys_dev.js

--- config/keys_dev.js
+++
@@ -1,8 +0,0 @@
-
-module.exports = {
-  mongoURI:
-    "mongodb+srv://ara:nfgYv89iVjpwq5iG@cluster0-q88yc.mongodb.net/test?retryWrites=true&w=majority",
-  secretOrKey: "AJP_mern_Project-02232020-&%$ghi^$FH"
-};
-
-//
