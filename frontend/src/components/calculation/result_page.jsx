bd46a3cd69498bbdadbaec35db7a51757a7d84f2
Phong Ngo <phongngo91@gmail.com>
2020-02-28 13:54:12 -0500

    Fix logout

--- frontend/src/components/calculation/result_page.jsx
+++ frontend/src/components/calculation/result_page.jsx
@@ -1,34 +1,47 @@
-import React from 'react';
-import './result.scss';
-import { Link } from 'react-router-dom';
-import NavBarContainer from '../nav/nav_container';
+import React from "react";
+import "./result.scss";
+import { Link } from "react-router-dom";
+import NavBarContainer from "../nav/nav_container";
 
 class ResultPage extends React.Component {
-  constructor(props){
+  constructor(props) {
     super(props);
 
-    this.state ={
+    this.state = {
       user_1: {
         zodiac: "Aquarius"
       },
-      user_2:{
+      user_2: {
         zodiac: "Aquarius"
-
       }
     };
   }
 
-
-  render(){
- 
+  render() {
     const { percentage } = this.props.calculation;
     const { sentence } = this.props.fortune;
     const { user_1, user_2 } = this.props.users;
 
-    if (user_2 === undefined || user_1 === undefined){
-      this.props.history.push('/');
+    let user2Component = null;
+
+    if (user_2) {
+      user2Component = (
+        <div className="user">
+          <img
+            className="user-zodiac-image"
+            src={`/${user_2.zodiac}.png`}
+            alt="user_2-zodiac"
+          />
+          <div className="user-zodiac">{user_2.zodiac}</div>
+          <div className="user-username">{user_2.username}</div>
+        </div>
+      );
     }
-    
+
+    if (user_2 === undefined || user_1 === undefined) {
+      this.props.history.push("/");
+    }
+
     return (
       <>
         <NavBarContainer />
@@ -48,16 +61,7 @@ class ResultPage extends React.Component {
               <div className="percentage">{percentage}</div>
               <div className="percent-mark">%</div>
             </div>
-
-            <div className="user">
-              <img
-                className="user-zodiac-image"
-                src={`/${user_2.zodiac}.png`}
-                alt="user_2-zodiac"
-              />
-              <div className="user-zodiac">{user_2.zodiac}</div>
-              <div className="user-username">{user_2.username}</div>
-            </div>
+            {user2Component}
           </div>
 
           <div className="result-sentence">
@@ -69,8 +73,9 @@ class ResultPage extends React.Component {
             />
           </div>
           <div className="play-button-div">
-
-            <Link className="play-again" to="/play">Play Again</Link>
+            <Link className="play-again" to="/play">
+              Play Again
+            </Link>
           </div>
         </div>
       </>
